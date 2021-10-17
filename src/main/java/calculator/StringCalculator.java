adacf83f081486ba4ef740b31933b6dde2082eb0
renukapetkar <renukaprtkar26@gmail.com>
2021-10-17 11:44:56 +0530

    second commit

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,14 +1,125 @@
 package calculator;
 
-class StringCalculator {
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+class StringCalculator {
+	
     public int add(String numbers) {
-    	if(numbers.isEmpty())
-    	{
+    	
+		if(numbers.isEmpty())
 			return 0;
-    	}
-		return 0;
-        
+		
+		else
+		{
+			String[] nums = tokenize(numbers);
+			int l = nums.length;
+			Vector<Integer> negatives = new Vector<>();
+			Vector<Integer> nos = new Vector<>();
+			String q = "";
+			int e =0;
+			int sum = 0;
+			
+			for(int i =0; i<l; i++)
+			{
+			
+					Matcher mx = Pattern.compile("^(\\+|-)?\\d+$").matcher(nums[i]);
+					while(mx.find())
+					{	
+						q = mx.group();
+						e = Integer.parseInt(q);
+						
+						if(e<0)
+						{
+							negatives.add(e);
+						}
+						if(e>1000)
+							e=0;
+							
+						nos.add(e);
+						sum +=e;
+					 }
+			}
+			
+			if(negatives.size() > 0)
+				throw new RuntimeException("Negatives not allowed: " + negatives);
+			
+			return sum;
+		}
+	}
+	
+	
+	
+	private static String[] tokenize(String str)
+	{
+
+		if(str.startsWith("//"))
+		{
+			if(str.startsWith("//["))
+				return splitMulCustomDelWithAnyLength(str);
+			else
+				return splitCustomDelimeter(str);	
+		}
+		
+			else
+			{
+				return splitNewlineAndCommas(str);	
+			}
+	}
+	
+
+	private static String[] splitNewlineAndCommas(String str)
+	{
+		String[] nums = str.split(",|\n");
+		return nums;
+	}
+	
+	private static String[] splitCustomDelimeter(String str)
+	{
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str);
+		m.matches();
+		String delimeter = m.group(1);
+		String nums = m.group(2);
+		return nums.split(delimeter);
+	}
+	
+	/*private static String[] splitCustomDelWithAnyLength(String str)
+	{
+		Matcher m = Pattern.compile("//(\\[.+\\])\n(.*)").matcher(str);
+		m.matches();
+		String delimeter = m.group(1);
+		String nums = m.group(2);
+		return nums.split(delimeter);
+	}
+	*/
+	
+	private static String[] splitMulCustomDelWithAnyLength(String str)
+	{
+		Matcher m = Pattern.compile("//(\\[.+\\])+\n(.*)").matcher(str);
+		m.matches();
+		String del = m.group(1);
+		String delimeters = new String();
+		
+		int l = del.length(),last =0;
+		for(int i =0; i<l ; i++)
+		{
+			if(del.charAt(i) == ']' && i != l-1)
+			{	
+				delimeters += del.substring(last,i);
+				delimeters += "]|";
+				last = i+1;
+				
+			}
+			
+			else if(i == l-1)
+				delimeters += del.substring(last,i) + "]";
+		}
+		String nums = m.group(2);
+		
+		return nums.split(delimeters); 
+		
+		
+	}
     }
 
-}
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -13,6 +13,55 @@ class StringCalculatorShould {
 		StringCalculator stringcalculator = new StringCalculator();
 		assertEquals(0, stringcalculator.add(""));
 	}
+	@Test
+	public void returnNumOnSingleArg()
+	{
+		StringCalculator stringcalculator = new StringCalculator();
+		assertEquals(1, stringcalculator.add("1"));
+	}
+	@Test
+	public void returnSumOnTwoArgsDelimitedByComma()
+	{
+		StringCalculator stringcalculator = new StringCalculator();
+		assertEquals(3, stringcalculator.add("1,2"));
+	}
+	
+	@Test
+	public void returnSumOnMultipleArgsDelimitedByComma()
+	{
+		StringCalculator stringcalculator = new StringCalculator();
+		assertEquals(6, stringcalculator.add("1,2,3"));
+	}
+
+	@Test
+	public void acceptNewlineAsValidDelimiter()
+	{
+		StringCalculator stringcalculator = new StringCalculator();
+		assertEquals(6, stringcalculator.add("1\n2,3"));
+	}
+	
+	@Test
+	public void customDelimeter()
+	{
+		StringCalculator stringcalculator = new StringCalculator();
+		assertEquals(3, stringcalculator.add("//;\n1;2"));
+	}
+	
+	@Test
+	public void raiseExceptionOnNegativeNumbers()
+	{
+		StringCalculator stringcalculator = new StringCalculator();
+		try {
+			stringcalculator.add("-1,-2,3");
+			fail("Exception expected.");
+		}
+		
+		catch(RuntimeException e)
+		{
+			assertEquals("Negatives not allowed: [-1, -2]", e.getMessage());
+		}
+	}
+	
 	
   
 }
